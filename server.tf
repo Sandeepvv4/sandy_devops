42c4d6046fe80d89af88f55eb46919b38a5a430f
Sandeepvv4 <122967421+Sandeepvv4@users.noreply.github.com>
2023-05-09 19:38:37 +1000

    Update server.tf
    Updated sg

--- server.tf
+++ server.tf
@@ -24,6 +24,13 @@ resource "aws_security_group" "server_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
